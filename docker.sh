3f8d0a5e63c5d13f61a80eede9ea139ee732b444
jeremiegerhardt <jeremie.gerhardt@irystec.com>
2018-05-07 16:56:20 -0400

    add condition to envirronment for opening browser

--- docker.sh
+++ docker.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Test environment
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+esac
+echo ${machine}
+
 # Exit this script if any error is encountered (e) and print every command (x)
 set -ex
 
@@ -21,7 +29,7 @@ then
   if docker ps --format='{{.ID}}' --no-trunc | grep -q "$cid"
   then
     read -rp "Container $name is already running. Restart it (potentially loosing work)? [yes/NO] "
-    if [[ ! ${REPLY^^} =~ ^(Y|YES)$ ]]
+    if [[ ! $REPLY =~ ^(y|yes|Y|YES)$ ]]
     then
       exit 1
     fi
@@ -31,9 +39,20 @@ then
 fi
 
 # Run the docker image inside a container and get its Container ID (CID)
-docker run --cidfile "$cidfile" -d -v "$DIR:/$name" "$name"
+docker run -p 8888:8888 --cidfile "$cidfile" -d -v "$DIR:/$name" "$name"
 
 # Inspect the running docker container to open a browser on its current IP
 cid="$(cat "$cidfile")"
-ipaddr="$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' "$cid")"
-xdg-open "http://$ipaddr:8888/"
+
+# Choose the right function to start browser according to envirronment
+if [[ $machine = Linux ]]
+then
+  xdg-open "http://localhost:8888/"
+  echo "votre machine est une ${machine}"
+fi
+
+if [[ $machine = Mac ]]
+then
+  open "http://localhost:8888/"
+  echo "votre machine est un ${machine}"
+fi
